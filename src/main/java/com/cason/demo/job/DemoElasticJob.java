a2267b3d4aa18f3d9fe938626ef4f2b720bb0cee
Casongorgor <116815480@qq.com>
2017-06-06 15:10:30 +0800

    替换用最新的elastic job

--- src/main/java/com/cason/demo/job/DemoElasticJob.java
+++ src/main/java/com/cason/demo/job/DemoElasticJob.java
@@ -1,18 +1,27 @@
 package com.cason.demo.job;
 
-import com.dangdang.ddframe.job.api.JobExecutionMultipleShardingContext;
-import com.dangdang.ddframe.job.plugin.job.type.simple.AbstractSimpleElasticJob;
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Created by jingle.huang on 2017/4/28.
  */
-public class DemoElasticJob extends AbstractSimpleElasticJob {
+//public class DemoElasticJob extends AbstractSimpleElasticJob {
+//    private Logger logger = LoggerFactory.getLogger(this.getClass());
+//
+//    @Override
+//    public void process(JobExecutionMultipleShardingContext jobExecutionMultipleShardingContext) {
+//        logger.info("开job lo， do something---------------jobExecutionMultipleShardingContext :[{}]",jobExecutionMultipleShardingContext);
+//    }
+//}
+public class DemoElasticJob implements SimpleJob {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+
     @Override
-    public void process(JobExecutionMultipleShardingContext jobExecutionMultipleShardingContext) {
-        logger.info("开job lo， do something---------------jobExecutionMultipleShardingContext :[{}]",jobExecutionMultipleShardingContext);
+    public void execute(ShardingContext shardingContext) {
+        logger.info("shardingContext------------------------");
     }
 }
